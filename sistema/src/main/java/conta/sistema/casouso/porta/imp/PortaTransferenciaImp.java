12725738e74ed186353e5b8d9e8d0752bcb7ef1c
Gabriel Augusto <biga00145@gmail.com>
2021-11-15 16:08:42 -0300

    Implementacao da classe PortaTransferenciaImp

--- sistema/src/main/java/conta/sistema/casouso/porta/imp/PortaTransferenciaImp.java
+++ sistema/src/main/java/conta/sistema/casouso/porta/imp/PortaTransferenciaImp.java
@@ -2,18 +2,66 @@ package conta.sistema.casouso.porta.imp;
 
 import conta.sistema.casouso.porta.PortaTransferencia;
 import conta.sistema.dominio.modelo.Conta;
+import static conta.sistema.dominio.modelo.Erro.inexistente;
+import static conta.sistema.dominio.modelo.Erro.mesmaConta;
+import static conta.sistema.dominio.modelo.Erro.obrigatorio;
+import conta.sistema.dominio.servico.Transferencia;
+import conta.sistema.porta.ContaRepositorio;
+import static java.util.Objects.isNull;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import java.math.BigDecimal;
 
+@Named
 public class PortaTransferenciaImp implements PortaTransferencia {
 
-    @Override
-    public Conta getConta(Integer numero) {
-        return null;
+    private ContaRepositorio repositorio;
+    private Transferencia transferencia;
+
+    @Inject
+    public PortaTransferenciaImp(ContaRepositorio repositorio, Transferencia transferencia) {
+        this.repositorio = repositorio;
+        this.transferencia = transferencia;
     }
 
     @Override
-    public void transferir(Integer contaDebito, Integer contaCredito, BigDecimal valorTransferencia) {
+    public Conta getConta(Integer numero) {
+        return this.repositorio.get(numero);
+    }
 
+    @Override
+    @Transactional
+    public void transferir(Integer contaDebito, Integer contaCredito, BigDecimal valorTransferencia) {
+        if (isNull(contaDebito)) {
+            obrigatorio("Conta débito");
+        }
+
+        if (isNull(contaCredito)) {
+            obrigatorio("Conta crédito");
+        }
+
+        if (isNull(valorTransferencia)) {
+            obrigatorio("Valor transferência");
+        }
+
+        final Conta ctDebito = this.repositorio.get(contaDebito);
+        if (isNull(ctDebito)) {
+            inexistente("Conta débito");
+        }
+
+        final Conta ctCredito = this.repositorio.get(contaCredito);
+        if (isNull(ctCredito)) {
+            inexistente("Conta crédito");
+        }
+
+        if (ctCredito.equals(ctDebito)) {
+            mesmaConta();
+        }
+
+        this.transferencia.transferir(valorTransferencia, ctDebito, ctCredito);
+        repositorio.alterar(ctDebito);
+        repositorio.alterar(ctCredito);
     }
 }
