0c318025b34a6242a8bc4531152c583c56e33776
Gabriel Augusto <biga00145@gmail.com>
2021-11-15 15:01:04 -0300

    Criacao da classe de TesteDebitoConta para testes

--- sistema/src/test/java/conta/sistema/dominio/modelo/TesteDebitoConta.java
+++ sistema/src/test/java/conta/sistema/dominio/modelo/TesteDebitoConta.java
@@ -1,15 +1,17 @@
 package conta.sistema.dominio.modelo;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 @DisplayName("Regra de débito de conta")
 class TesteDebitoConta {
 
-    private BigDecimal cem = new BigDecimal(100);
+    private final BigDecimal cem = new BigDecimal(100);
     private Conta contaValida;
 
     @BeforeEach
@@ -18,7 +20,21 @@ class TesteDebitoConta {
     }
 
     @Test
-    void execute() {
-        String x = "";
+    @DisplayName("valor debito nulo como obrigatório")
+    void teste1() {
+        Assertions.assertThrows(NegocioException.class, () -> contaValida.debitar(null), "Valor débito é obrigatório.");
+    }
+
+    @Test
+    @DisplayName("valor debito menor ou igual a 0")
+    void teste2() {
+        Assertions.assertThrows(NegocioException.class, () -> contaValida.debitar(BigDecimal.ZERO), "Valor débito é obrigatório.");
+    }
+
+    @Test
+    @DisplayName("valor debito debitado com sucesso")
+    void teste3() {
+        contaValida.debitar(BigDecimal.TEN);
+        assertEquals(new BigDecimal(90), contaValida.getSaldo());
     }
 }
