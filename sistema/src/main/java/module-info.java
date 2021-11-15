8dd73485504954624b7f2259565aaaf385685e1e
Gabriel Augusto <biga00145@gmail.com>
2021-11-15 16:53:38 -0300

    Config: Module-Info

--- sistema/src/main/java/module-info.java
+++ sistema/src/main/java/module-info.java
@@ -1,17 +1,23 @@
 module conta.sistema {
 
+    // usa spring
     requires javax.inject;
     requires spring.tx;
 
     // expondo porta de entrada (driver)
-
     exports conta.sistema.casouso.porta;
 
     // expondo sistema negocio
-
     exports conta.sistema.dominio.servico;
+    exports conta.sistema.dominio.modelo;
 
     // expondo adaptadores de saídas (driven)
-
+    exports conta.sistema.porta;
     exports conta.adaptador;
+
+    // abre reflexao spring
+    opens conta.sistema.casouso.porta;
+    opens conta.sistema.dominio.servico;
+    opens conta.adaptador;
+
 }
