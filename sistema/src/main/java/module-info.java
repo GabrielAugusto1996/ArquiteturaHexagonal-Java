4ea8b445ccec547c7006efc25dad0132beac5d09
Gabriel Augusto <biga00145@gmail.com>
2021-11-15 16:17:47 -0300

    Config. Java Module

--- sistema/src/main/java/module-info.java
+++ sistema/src/main/java/module-info.java
@@ -2,4 +2,16 @@ module conta.sistema {
 
     requires javax.inject;
     requires spring.tx;
+
+    // expondo porta de entrada (driver)
+
+    exports conta.sistema.casouso.porta.imp;
+
+    // expondo sistema negocio
+
+    exports conta.sistema.dominio.servico;
+
+    // expondo adaptadores de saídas (driven)
+
+    exports conta.adaptador;
 }
